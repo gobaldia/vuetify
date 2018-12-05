705cd3d3f136b1d416801d998d1fa6a473cc3ead
Gabriel Obaldía <gobaldia@gmail.com>
2018-12-05 09:34:52 -0300

    Add support for Spanish language

---
+++ packages/vuetify/src/locale/es.ts
@@ -0,0 +1,14 @@
+export default {
+  dataIterator: {
+    rowsPerPageText: 'Elementos por página:',
+    rowsPerPageAll: 'Todos',
+    pageText: '{0}-{1} de {2}',
+    noResultsText: 'Ningún elemento encontrado',
+    nextPage: 'Próxima página',
+    prevPage: 'Página anterior'
+  },
+  dataTable: {
+    rowsPerPageText: 'Lneas por página:'
+  },
+  noDataText: 'No hay datos disponibles'
+}
